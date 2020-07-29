dc40d8c5b4826f552c63b56045bb5073696671af
hypensanez12 <46888854+hypensanez12@users.noreply.github.com>
2020-07-30 00:19:06 +0100

    Assets Added

---
+++ UnityCharacterAnimationsTest/Assets/Scripts/CameraController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public float mouseSensitivity = 150f;
+    public Transform cameraAxis;
+    public Transform player;
+    float xRotation = 0f;
+    float yRotation = 0f;
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        //setting the upwards/downwards rotaion to the mouse up and down movement
+        xRotation += mouseY;
+        //stopping it from rotating in circles over the player
+        xRotation = Mathf.Clamp(xRotation, -40, 10);
+
+        //setting left and right rotation to the left and right mouse movement
+        yRotation += mouseX;
+
+        //making the camera rotate with the previous variables
+        cameraAxis.rotation = Quaternion.Euler(xRotation, yRotation, 0f);
+        player.Rotate(Vector3.up * mouseX);
+    }
+}

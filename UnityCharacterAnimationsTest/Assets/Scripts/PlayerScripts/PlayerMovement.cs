53f476422546726f669cb9a383af94169e170e91
hypensanez12 <46888854+hypensanez12@users.noreply.github.com>
2020-08-06 22:54:35 +0100

    GreyBoxing Part 2

--- UnityCharacterAnimationsTest/Assets/Scripts/PlayerScripts/PlayerMovement.cs
+++ UnityCharacterAnimationsTest/Assets/Scripts/PlayerScripts/PlayerMovement.cs
@@ -7,13 +7,18 @@ public class PlayerMovement : MonoBehaviour
     public CharacterController controller;
     public float speed;
     public float baseSpeed;
-    Vector3 velocity;
+    Vector3 MoveVector;
+    Vector3 PreviousJump;
     public bool Onground;
     public Transform Ground;
     public float grounDistance;
     public float fallSpeed;
-    public float jumpHeight;
+    public float JumpForce;
+    float verticalVelocity;
+    float Gravity = 25;
+
     public LayerMask groundMask;
+
     
 
     void Start()
@@ -26,6 +31,28 @@ public class PlayerMovement : MonoBehaviour
     {
         Onground = Physics.CheckSphere(Ground.position, grounDistance,groundMask);
         Move();
+
+        Jump();
+
+        if(controller.isGrounded)
+        {
+            verticalVelocity = -1;
+
+            if(Input.GetKeyDown(KeyCode.A))
+            {
+                verticalVelocity = JumpForce;
+            }
+        }
+        else
+        {
+            verticalVelocity -= Gravity * Time.deltaTime;
+            MoveVector = PreviousJump;
+        }
+
+        MoveVector.y = 0;
+        MoveVector.Normalize();
+        MoveVector *= speed;
+        MoveVector.y = verticalVelocity;
     }
 
     
@@ -34,10 +61,10 @@ public class PlayerMovement : MonoBehaviour
     {
         if(Input.GetButtonDown("Jump") && Onground)
         {
-            velocity.y = Mathf.Sqrt(jumpHeight * -2f * fallSpeed);
+            MoveVector.y = Mathf.Sqrt(JumpForce * -2f * fallSpeed);
         }
 
-        Debug.Log(jumpHeight);
+        
     }
 
     void Move()
@@ -45,9 +72,9 @@ public class PlayerMovement : MonoBehaviour
         float h = Input.GetAxis("Horizontal");
         float v = Input.GetAxis("Vertical");
 
-        if (Onground && velocity.y < 0)
+        if (Onground && MoveVector.y < 0)
         {
-            velocity.y = -2f;
+            MoveVector.y = -2f;
         }
 
         //horizontal movement
@@ -55,11 +82,27 @@ public class PlayerMovement : MonoBehaviour
         controller.Move(move * speed * Time.deltaTime);
 
         //vertical velocity
-        velocity.y += fallSpeed * Time.deltaTime;
-        controller.Move(velocity * Time.deltaTime);
+        MoveVector.y += fallSpeed * Time.deltaTime;
+        controller.Move(MoveVector * Time.deltaTime);
 
         
     }
 
-    
+    private void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        if(!controller.isGrounded && hit.normal.y < 0.1f)
+        {
+            
+            if (Input.GetKeyDown(KeyCode.A)|| Input.GetKeyDown(KeyCode.D))
+            {
+                Debug.DrawRay(hit.point, hit.normal, Color.red, 1.30f);
+                verticalVelocity = JumpForce;
+                //Jump();
+                MoveVector = hit.normal * speed;
+            }
+            
+        }
+
+        
+    }
 }
